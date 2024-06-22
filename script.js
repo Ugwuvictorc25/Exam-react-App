57e1c5b32fdd4a7c85f8d2f351e3dbf4ee1076dd
Ugwu Victor Chika <ugwuvictor2097.com>
2024-06-22 14:08:36 +0100

    deleted git file

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+
+const bar = document.querySelector("#bar");
+const navBar = document.querySelector(".navbar");
+const cancel = document.querySelector(".fa-times");
+
+if (bar) {
+	bar.addEventListener("click", function () {
+		navBar.classList.add("active");
+		document.querySelector(".fa-shopping-bag").classList.add("hide");
+	});
+}
+
+cancel.addEventListener("click", function () {
+	navBar.classList.remove("active");
+});
